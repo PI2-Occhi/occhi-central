2bb0a7bb43c034686de4b6aef5fbbf427387a17b
Marcos N <marcosnery.comp@gmail.com>
2022-05-06 00:56:37 -0300

    adding configuration class

---
+++ occhi_central/config.py
@@ -0,0 +1,34 @@
+import json
+
+
+class Dict(dict):
+    __getattr__ = dict.get
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
+
+
+class Configuration(object):
+    """
+    Reads from JSON configuration file to setup environment
+
+    """
+
+    @staticmethod
+    def __load__(data):
+        if type(data) is dict:
+            return Configuration.load_dict(data)
+        else:
+            return data
+
+    @staticmethod
+    def load_dict(data: dict):
+        result = Dict()
+        for key, value in data.items():
+            result[key] = Configuration.__load__(value)
+        return result
+
+    @staticmethod
+    def load_json(path: str):
+        with open(path, "r") as f:
+            result = Configuration.__load__(json.loads(f.read()))
+        return result
