4954ae7d4388820f988f5308a42b293fca39a330
Marcos N <marcosnery.comp@gmail.com>
2022-05-06 00:56:49 -0300

    improving mqtt class

--- occhi_central/mqtt.py
+++ occhi_central/mqtt.py
@@ -1,107 +1,60 @@
-import random
-import time
 from paho.mqtt import client as mqtt_client
+import random
 import json
 
-broker = '192.168.0.70'
-port = 1883
-subscribe_topic1 = "/sensors/#"
-subscribe_topic2 = "/movement/app"
-publishing_topic = "/movement/instructions"
-client_id = f'central_server_{random.randint(0, 100)}'
 
+class MQTTClient:
+    """
+    Client to send MQTT messages to movement topic
+    and to subscribe to sensors topic and foward messages to BLE
+    """
 
-def connect_mqtt() -> mqtt_client:
-  def on_connect(client, userdata, flags, rc):
-    if rc != 0:
-      print("Failed to connect, return code %d\n", rc)
+    def __init__(self, broker, port, sensors_topic, movement_topic, send_ble_message):
+        self.client_id = f"central_server_{random.randint(0, 100)}"
+        self.broker = broker
+        self.port = port
+        self.sensors_topic = sensors_topic
+        self.movement_topic = movement_topic
+        self.send_ble_message = send_ble_message
 
-  client = mqtt_client.Client(client_id)
-  client.on_connect = on_connect
-  client.connect(broker, port)
-  return client
+    def connect_mqtt(self) -> mqtt_client:
+        def on_connect(client, userdata, flags, rc):
+            if rc != 0:
+                print("Failed to connect, return code %d\n", rc)
 
-def publish(client, message):
-  result = client.publish(publishing_topic, message)
-  status = result[0]
-  if status == 0:
-    print(f"Comando `{message}` enviado ao MQTT`")
-  else:
-    print(f"Failed to command: {message}")
+        client = mqtt_client.Client(self.client_id)
+        client.on_connect = on_connect
+        client.connect(self.broker, self.port)
+        return client
 
-def subscribe(client: mqtt_client):
-  def on_message(client, userdata, msg):
-    if('sensors' in msg.topic):
-      print("Repassando estado do sensor: {}".format(str(msg.payload.decode())))
-    if('/movement/app' == msg.topic):
-      command = str(msg.payload.decode())
-      if command == "FW":
-        print("Recebido comando, para frente.")
-        command = json.dumps({
-            'forward': True,
-            'backward': False,
-            'left': False,
-            'right': False,
-            'stop': False,
-            'turn': False,
-        })
-      if command == "BW":
-        print("Recebido comando, para trás.")
-        command = json.dumps({
-            'forward': False,
-            'backward': True,
-            'left': False,
-            'right': False,
-            'stop': False,
-            'turn': False,
-        })     
-      if command == "RT":
-        print("Recebido comando, para direita.")
-        command = json.dumps({
-            'forward': False,
-            'backward': False,
-            'left': False,
-            'right': True,
-            'stop': False,
-            'turn': False,
-        })
-      if command == "LT":
-        print("Recebido comando, para esquerda.")
-        command = json.dumps({
-            'forward': False,
-            'backward': False,
-            'left': True,
-            'right': False,
-            'stop': False,
-            'turn': False,
-        })
-      if command == "ST":
-        print("Recebido comando, parar.")
-        command = json.dumps({
-            'forward': False,
-            'backward': False,
-            'left': False,
-            'right': False,
-            'stop': True,
-            'turn': False,
-        })
-      if command == "TR":
-        print("Recebido comando, virar 180 graus.")
-        command = json.dumps({
-            'forward': False,
-            'backward': False,
-            'left': False,
-            'right': False,
-            'stop': False,
-            'turn': True,
-        })
-      publish(client, command)
+    def publish(self, client, message):
+        result = client.publish(self.movement_topic, message)
+        status = result[0]
+        if status == 0:
+            print(f"Comando `{message}` enviado ao MQTT`")
+        else:
+            print(f"Falha ao enviar comando: {message}")
 
-  client.subscribe(subscribe_topic1)
-  client.subscribe(subscribe_topic2)
-  client.on_message = on_message
+    def subscribe(self, client: mqtt_client):
+        def on_message(client, userdata, msg):
+            if "sensors" in msg.topic:
+                print(
+                    "Repassando estado do sensor: {}".format(str(msg.payload.decode()))
+                )
 
-def run_mqtt():
-  client = connect_mqtt()
-  subscribe(client)
-  client.loop_forever()
+                if "power" in msg.topic:
+                    self.send_ble_message("power", msg)
+                elif "overturn" in msg.topic:
+                    self.send_ble_message("overturn", msg)
+                elif "chair" in msg.topic:
+                    self.send_ble_message("chair", msg)
+                elif "colision" in msg.topic:
+                    self.send_ble_message("colision", msg)
+
+        client.subscribe(self, sensors_topic)
+        client.on_message = on_message
+
+    def run_mqtt(self):
+        client = connect_mqtt()
+        subscribe(client)
+        client.loop_forever()
